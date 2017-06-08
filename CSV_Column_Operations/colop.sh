46e9f78fd7b8c2933fcb045b118950353d1fadab
Srishti Belwariar <sb2355@cornell.edu>
2017-06-07 23:59:04 -0400

    header updates

--- CSV_Column_Operations/colop.sh
+++ CSV_Column_Operations/colop.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env bash
+#title          : colop.sh
+#description    : This bash script can be used to add, multiply, subtract, or divide 
+#                 specific column values in each row and input the solutions for 
+#                 each row into a new Sol.txt file.
+#author         : Srishti Belwariar
+#date           : 03/23/2017  
+#usage          : ./colop.sh [CSVFileName] [operationcode] [columnnumber] [columnnumber] ... 
+#notes          : 
+#   CSV file must be a valid existing file, preferably in same directory as script
+#   operation codes are as follows: add for addition, sub for subtraction, 
+#       mul for multiplication, and div for division
+#   columnnumber represents the column index (integer), with the leftmost column having
+#       an index of 1.
+#   valid execution of this command means having at least four arguments, with the
+#       first being the csv file name, second being the sole operation, and at least two
+#       integers referring to columns in the file.  Note: more than 2 columns may be 
+#       referenced in the command.
+#   some valid commands, if the csv files were real:
+#   $ ./colop.sh test.csv add 3 4
+#   $ ./colop.sh cakesales.csv sub 4 6 2
+#   $ ./colop.sh orders.csv mul 3 7 2 1 2
+#   $ ./colop.sh test.csv div 3 2
+#bash_version   : 3.2.57(1)-release
+
+#============================================================================
 
 #PC: no less than 4 arguments
 #PC: first argument is file name of csv file type
