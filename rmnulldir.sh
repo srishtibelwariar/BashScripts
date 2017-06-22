e79d269ffec831b31471c31c03d2a98bdcb87830
Srishti Belwariar <sb2355@cornell.edu>
2017-06-22 13:16:40 -0400

    Add files via upload

---
+++ rmnulldir.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#title          : rmnulldir.sh
+#description    : This bash script deletes empty subdirectories in main directory
+#		  and echos number of subdirectories deleted.
+#author         : Srishti Belwariar
+#date           : 06/19/2017
+#usage          : ./rmnulldir.sh
+#notes          : This script deletes empty subdirectories that lie within the root directory's
+#		 	 first tier of subdirectories within the main directory as well as
+#		 	 subdirectories within those folders.
+#		  Script echos the number of folders deleted
+#		
+#		Valid commands:
+#		  if script is in the same directory referenced
+#			$ ./rmnulldir.sh
+#bash_version   : 3.2.57(1)-release
+#============================================================================
+
+dir=(*/)
+counter=0
+
+for x in "${dir[@]}";
+do
+size=$(du "$x" | cut -f1)
+
+if [ $size -eq 0 ];
+then 
+rmdir "$x"
+counter=$(($counter+1))
+fi
+done
+
+echo "Deleted $counter subdirectories"
+
+ 
